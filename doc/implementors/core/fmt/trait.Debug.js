08cd1bb9a95bf272210a40b717e359049fe916b8
Benjamin Dykstra <bendykst@gmail.com>
2015-10-13 15:17:13 -0500

    Updated docs for 0.3.0

--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['simplemad'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='simplemad/enum.MadError.html' title='simplemad::MadError'>MadError</a>",];
+implementors['simplemad'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='simplemad/enum.MadError.html' title='simplemad::MadError'>MadError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
