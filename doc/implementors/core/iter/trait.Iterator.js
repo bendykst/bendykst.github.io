a86801a101674d4ef267e4dc7f900e5bead19c36
Benjamin Dykstra <bendykst@gmail.com>
2015-10-19 23:08:55 -0500

    Updated documentation for simplemad v0.6.0

--- doc/implementors/core/iter/trait.Iterator.js
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['simplemad'] = ["impl&lt;R&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='simplemad/struct.Decoder.html' title='simplemad::Decoder'>Decoder</a>&lt;R&gt; <span class='where'>where R: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> + <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> + 'static</span>",];
+implementors['simplemad'] = ["impl&lt;R&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='simplemad/struct.Decoder.html' title='simplemad::Decoder'>Decoder</a>&lt;R&gt; <span class='where'>where R: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/simplemad/sidebar-items.js
+++ doc/simplemad/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SimplemadError","An error encountered during the decoding process"]],"struct":[["Decoder","An interface for the decoding operation"],["Frame","A decoded frame"]]});
+initSidebarItems({"enum":[["SimplemadError","An error encountered during the decoding process"]],"struct":[["Decoder","An interface for the decoding operation"],["Frame","A decoded frame"],["MadFixed32","libmad's native fixed-point format"]]});
