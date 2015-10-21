b7e39629d8b2cef77b19e6a6836f259cdf4c1bad
Benjamin Dykstra <bendykst@gmail.com>
2015-10-21 11:42:08 -0500

    Fixed typo

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['simplemad_sys'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='simplemad_sys/enum.MadError.html' title='simplemad_sys::MadError'>MadError</a>",];
+implementors['simplemad_sys'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='simplemad_sys/enum.MadError.html' title='simplemad_sys::MadError'>MadError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='simplemad_sys/enum.MadLayer.html' title='simplemad_sys::MadLayer'>MadLayer</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='simplemad_sys/enum.MadMode.html' title='simplemad_sys::MadMode'>MadMode</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
